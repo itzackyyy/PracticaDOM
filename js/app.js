75530fd08937228bb2ac074bf56bdef7e0144467
itzackyyy <brun.soto@duocuc.cl>
2025-10-31 02:33:48 -0300

    Se integra un archivo .js para crear la interacción hacia el DOM del HTML y se explica para qué sirve en general mediante comentarios :)

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let div = document.querySelector('div')
+//  inicializamos la variable div con "let" 
+//  este div esta ligado con el main del HTML,
+//  ¿que quiere decir? pues que estamos accediendo a
+//  nuestro DOM para poder modificarlo desde nuestro
+// JavaScript, es decir, desde aqui.
+
+
+
+// aqui está el function 
+function colores(){
+    div.innerHTML= "Esto proviene desde el archivo JS"
+    div.style.marginTop= "50px"
+    div.style.background = "aliceblue";
+}
